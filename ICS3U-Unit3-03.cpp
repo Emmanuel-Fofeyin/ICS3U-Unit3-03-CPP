5d9616643c6bae969f022d98587dc356db56426a
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-10-18 01:48:35 +0000

    major changes have bin imbedded aggressively!!

---
+++ ICS3U-Unit3-03.cpp
@@ -0,0 +1,34 @@
+#// // Copyright (c) 2022 St.Emmanuel Fofeyin All rights reserved.
+
+// Created by : Emmanuel
+// Created on : Oct 2022
+// This program is a number guessing game using an else statement
+
+#include <iostream>
+#include <random>
+
+int main() {
+    // creating variables
+    int randomNumber;
+    int guessNumber;
+
+    // generating random number
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());  // mersenne_twister
+    std::uniform_int_distribution<int> idist(0,9);
+    randomNumber = idist(rgen);
+
+    // input
+    std::cout << "Guess a number from 0 - 9: ";
+    std::cin >> guessNumber;
+
+    // process and output
+    if (randomNumber == guessNumber) {
+        std::cout << "\nYou  guessed correctly.";
+    } else { 
+        std::cout << "\nIncorrect, the number was: "
+        << randomNumber << ".";
+    }
+
+    std::cout << "\n\n\nDone.\n";
+}
